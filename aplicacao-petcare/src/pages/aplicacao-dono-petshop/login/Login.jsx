611b0eeff6de658d444c5bea661d6b7b7990041d
Samuel Galdino dos Santos <samuel.santos@sptech.school>
2024-10-09 14:18:09 -0300

    feat: continuação da criação das inputs da tela de login

--- aplicacao-petcare/src/pages/aplicacao-dono-petshop/login/Login.jsx
+++ aplicacao-petcare/src/pages/aplicacao-dono-petshop/login/Login.jsx
@@ -7,33 +7,36 @@ const Login = () => {
   return (
     <div>
       <MainHeader />
-     <div className={styles.container}>
-      <div className={styles.loginBox}>
-        <h2>Faça seu login!</h2>
-        <form>
-          <div className={styles.inputGroup}>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Digite seu email" />
+      <div className={styles.container}>
+        <div className={styles.loginBox}>
+          <h2>Faça seu login!</h2>
+          <form>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Digite seu email" />
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">Senha</label>
+              <input type="password" id="password" placeholder="Digite sua senha" />
+            </div>
+            <button type="submit" className={styles.loginButton}>Entrar</button>
+          </form>
+          <div className={styles.footer}>
+            <a href="/" className={styles.forgotPassword}>Esqueceu a senha?</a>
+            <a href="/" className={styles.voltarButton}>Voltar</a>
           </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="password">Senha</label>
-            <input type="password" id="password" placeholder="Digite sua senha" />
+          <p className={styles.helpText}>
+            Enfrentando dificuldades para fazer login? Este portal de acesso é exclusivo para donos de petshops. Se você é um tutor de pet, acesse a PetCare por este <a href="/">site</a>.
+          </p>
+          <div className={styles.footer}>
+            <a href="/" className={styles.helpText2}>Ainda não é cliente?</a>
+            <button className={styles.registerButton}>COMECE AGORA</button>
           </div>
-          <button type="submit" className={styles.loginButton}>Entrar</button>
-        </form>
-        <div className={styles.footer}>
-          <a href="/" className={styles.forgotPassword}>Esqueceu a senha?</a>
-          <a href="/" className={styles.voltarButton}>Voltar</a>
         </div>
-        <p className={styles.helpText}>
-          Enfrentando dificuldades para fazer login? Este portal de acesso é exclusivo para donos de petshops. Se você é um tutor de pet, acesse a PetCare por este <a href="/">site</a>.
-        </p>
-        <button className={styles.registerButton}>COMECE AGORA</button>
+        <div className={styles.imageBox}>
+          <img src={petImagem} alt="Pet and owner high five" />
+        </div>
       </div>
-      <div className={styles.imageBox}>
-        <img src={petImagem} alt="Pet and owner high five" />
-      </div>
-    </div>
     </div>
   );
 };
