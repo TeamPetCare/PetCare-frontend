9d141796cb1fdf03ac63fada2f812461348710ae
Samuel Galdino dos Santos <samuel.santos@sptech.school>
2024-10-10 19:16:40 -0300

    feat: continuação tela de inicio

--- aplicacao-petcare/src/pages/aplicacao-dono-petshop/login/Login.jsx
+++ aplicacao-petcare/src/pages/aplicacao-dono-petshop/login/Login.jsx
@@ -3,6 +3,11 @@ import styles from '../login/Login.module.css';
 import petImagem from '../../../utils/assets/login/imagem-pet-login.png';
 import MainHeader from '../../../components/site-institucional/shared/MainHeader/MainHeader';
 
+// Importando os ícones
+import { FaUserAlt } from "react-icons/fa";
+import { IoLockClosed } from "react-icons/io5";
+import { PiEyeSlashFill } from "react-icons/pi";
+
 const Login = () => {
   return (
     <div>
@@ -11,13 +16,24 @@ const Login = () => {
         <div className={styles.loginBox}>
           <h2>Faça seu login!</h2>
           <form>
+            {/* Grupo de input do email com ícone */}
             <div className={styles.inputGroup}>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" placeholder="Digite seu email" />
+              <div className={styles.inputWrapper}>
+                {/* Ícone de usuário */}
+                <FaUserAlt className={styles.icon} />
+                <input type="email" id="email" placeholder="Digite seu email" />
+              </div>
             </div>
+
+            {/* Grupo de input de senha com ícone */}
             <div className={styles.inputGroup}>
-              <label htmlFor="password">Senha</label>
-              <input type="password" id="password" placeholder="Digite sua senha" />
+              <div className={styles.inputWrapper}>
+                {/* Ícone de cadeado */}
+                <IoLockClosed className={styles.icon} />
+                <input type="password" id="password" placeholder="Digite sua senha" />
+                {/* Ícone de "olho" para visualização da senha */}
+                <PiEyeSlashFill className={styles.eyeIcon} />
+              </div>
             </div>
             <button type="submit" className={styles.loginButton}>Entrar</button>
           </form>
@@ -26,7 +42,7 @@ const Login = () => {
             <a href="/" className={styles.voltarButton}>Voltar</a>
           </div>
           <p className={styles.helpText}>
-            Enfrentando dificuldades para fazer login? Este portal de acesso é exclusivo para donos de petshops. Se você é um tutor de pet, acesse a PetCare por este <a href="/">site</a>.
+            <p className={styles.negrito}>Enfrentando dificuldades para fazer login?</p>Este portal de acesso é exclusivo para donos de petshops. Se você é um tutor de pet, acesse a PetCare por este <a href="/">site</a>.
           </p>
           <div className={styles.footer}>
             <a href="/" className={styles.helpText2}>Ainda não é cliente?</a>
