5846648fc8884c73fc5f1d6c1a0a2a3f243d5f0f
daniescobarx <daniela.mollo@sptech.school>
2024-10-11 15:42:01 -0300

    feat: add section inicial pag sobre nós

--- aplicacao-petcare/src/pages/site-institucional/sobreNos/SobreNos.jsx
+++ aplicacao-petcare/src/pages/site-institucional/sobreNos/SobreNos.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+import styles from "./SobreNos.module.css"; // Estilos para SobreNos
+import SectionSobreNos from "../../../components/site-institucional/sobrenos/SectionSobreNos.jsx"; 
+import imagem from "../../../utils/assets/site-institucional/sobrenos/mulhersobrenos.svg";
+
 const SobreNos = () => {
   return (
-    <div>
-      /* seu código aqui */
+    <div className={styles.sobreNos}>
+      <SectionSobreNos
+        titulo="A paixão e o cuidado"
+        titulo2=" que seu pet merece."
+        textoDetalhado="Conheça nossa missão de oferecer serviços excepcionais e um atendimento personalizado que faz a diferença na vida do seu bichinho."
+        imagem={imagem}
+        valores="Nossos valores:"
+        valores1="Simplicidade e Usabilidade"
+        valores2="Foco no Cliente"
+        valores3="Inovação"
+        valores4="Cuidado e Bem-Estar Animal"
+      />
     </div>
   );
 };
