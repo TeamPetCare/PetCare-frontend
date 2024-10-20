a00f86d168c9387dc8ff55bb48795d34b71508fc
BeatrizGBrandao <beatriz.brandao@sptech.school>
2024-10-20 17:30:36 -0300

    feat: kpis e calendário da tela de inicio

--- aplicacao-petcare/src/components/aplicacao-dono-petshop/inicio/calendario/Calendario.jsx
+++ aplicacao-petcare/src/components/aplicacao-dono-petshop/inicio/calendario/Calendario.jsx
@@ -1,166 +1,75 @@
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import ptBR from "date-fns/locale/pt-BR"; 
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import React, { useState, useEffect } from "react";
+import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import "./Calendario.module.css";
-import Event from "./event/Event";
+import styles from "./Calendario.module.css";
+import Event from "./components/Event.jsx";
+import { localizer } from "./config/localizer";
+import { formats } from "./config/formats";
+import { messages } from "./config/messages";
 
-const locales = {
-  "pt-BR": ptBR, 
-};
+function Calendario({ dadosAgendamentos }) {
+  const [allEvents, setAllEvents] = useState(dadosAgendamentos);
+  const [view, setView] = useState("month");
+  const [eventsOnDate, setEventsOnDate] = useState([]);
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek: () => startOfWeek(new Date(), { locale: ptBR }),
-  getDay,
-  locales,
-});
+  // Função para excluir um evento
+  const handleDeleteEvent = (eventId) => {
+    setAllEvents((prevEvents) =>
+      prevEvents.filter((event) => event.id !== eventId)
+    );
+  };
 
-const messages = {
-  allDay: "Dia inteiro",
-  previous: "Anterior",
-  next: "Próximo",
-  today: "Hoje",
-  month: "Mês",
-  week: "Semana",
-  day: "Dia",
-  agenda: "Agenda",
-  date: "Data",
-  time: "Hora",
-  event: "Evento",
-  noEventsInRange: "Nenhum evento neste período.",
-  showMore: (total) => `+ Ver mais (${total})`,
-};
+  const handleUpdateEvent = (updatedEvent) => {
+    setAllEvents((prevEvents) => {
+      const updatedEvents = prevEvents.map((event) =>
+        event.id === updatedEvent.id ? updatedEvent : event
+      );
+      console.log("Eventos após atualização:", updatedEvents); // Log para depuração
+      return updatedEvents;
+    });
+  };
 
-const formats = {
-  dateFormat: "dd", // Exibe o dia do mês (ex: 01, 02)
-  dayFormat: (date, culture, localizer) =>
-    format(date, "EEEE", { locale: ptBR }), // Exibe o nome completo do dia da semana (ex: segunda-feira)
-  weekdayFormat: (date, culture, localizer) =>
-    format(date, "EEEE", { locale: ptBR }), // Exibe o nome do dia da semana no cabeçalho (ex: segunda)
-  monthHeaderFormat: (date, culture, localizer) =>
-    format(date, "MMMM yyyy", { locale: ptBR }), // Exibe o mês e o ano no cabeçalho (ex: Julho 2021)
-  dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
-    `${format(start, "dd MMM", { locale: ptBR })} - ${format(end, "dd MMM", {
-      locale: ptBR,
-    })}`, // Exibe o intervalo de dias
-};
+  const handleShowMore = (events, date) => {
+    setEventsOnDate(events);
+  };
 
-const events = [
-  {
-    title: "Big Meeting",
-    paymentStatus: "Pago",
-    paymentMethod: "Pix",
-    allDay: true,
-    hourStart: new Date(2024, 9, 1, 9, 0), // Mês 9 para Outubro
-    hourEnd: new Date(2024, 9, 1, 17, 0), // Mês 9 para Outubro
-    start: new Date(2024, 9, 1), // Mês 9 para Outubro
-    end: new Date(2024, 9, 2), // Mês 9 para Outubro
-  },
-  {
-    title: "Big Meeting 2",
-    paymentStatus: "Pendente",
-    paymentMethod: "Dinheiro",
-    hourStart: new Date(2024, 9, 1, 9, 0), // Mês 9 para Outubro
-    hourEnd: new Date(2024, 9, 1, 17, 0), // Mês 9 para Outubro
-    start: new Date(2024, 9, 1), // Mês 9 para Outubro
-    end: new Date(2024, 9, 2), // Mês 9 para Outubro
-  },
-  {
-    title: "Vacation",
-    paymentStatus: "Pago",
-    paymentMethod: "Cartão de Crédito",
-    hourStart: new Date(2024, 9, 7, 9, 0), // Mês 9 para Outubro
-    hourEnd: new Date(2024, 9, 7, 17, 0),
-    start: new Date(2024, 9, 7), // Mês 9 para Outubro
-    end: new Date(2024, 9, 10), // Mês 9 para Outubro
-  },
-  {
-    title: "Conference",
-    paymentStatus: "Pendente",
-    paymentMethod: "Pix",
-    hourStart: new Date(2024, 9, 1, 9, 0), // Mês 9 para Outubro
-    hourEnd: new Date(2024, 9, 1, 17, 0),
-    start: new Date(2024, 9, 20), // Mês 9 para Outubro
-    end: new Date(2024, 9, 23), // Mês 9 para Outubro
-  },
-];
-
-function Calendario() {
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-  const [allEvents, setAllEvents] = useState(events);
-
-  function handleAddEvent() {
-    for (let i = 0; i < allEvents.length; i++) {
-      const d1 = new Date(allEvents[i].start);
-      const d2 = new Date(newEvent.start);
-      const d3 = new Date(allEvents[i].end);
-      const d4 = new Date(newEvent.end);
-
-      if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
-        alert("Conflito de Eventos");
-        break;
-      }
-    }
-    setAllEvents([...allEvents, newEvent]);
-  }
+  // useEffect para verificar alterações em allEvents
+  useEffect(() => {
+    // Este efeito pode ser usado para outras atualizações ou side effects, se necessário
+    console.log("Eventos atualizados:", allEvents);
+  }, [allEvents]);
 
   return (
-    <div className="Calendario">
-      {/* <h1>Calendar</h1>
-            <h2>Add New Event</h2>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Add Title"
-                    style={{ width: "20%", marginRight: "10px" }}
-                    value={newEvent.title}
-                    onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
-                />
-                <DatePicker
-                    placeholderText="Start Date"
-                    style={{ marginRight: "10px" }}
-                    selected={newEvent.start}
-                    onChange={(start) => setNewEvent({ ...newEvent, start })}
-                />
-                <DatePicker
-                    placeholderText="End Date"
-                    selected={newEvent.end}
-                    onChange={(end) => setNewEvent({ ...newEvent, end })}
-                />
-                <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
-                    Add Event
-                </button>
-            </div> */}
+    <div className={styles["calendario"]}>
       <Calendar
         messages={messages}
-        formats={formats}
         localizer={localizer}
         events={allEvents}
         startAccessor="start"
         endAccessor="end"
-        min={new Date(2024, 0, 1, 8, 0)}
-        max={new Date(2024, 0, 1, 20, 0)}
         step={30}
         style={{
           height: "90vh",
-          width: "79%",
-          color: "blue",
+          color: "var(--cor-primaria-azul-escuro)",
           marginTop: "-33px",
           fontSize: "14px",
           fontWeight: 700,
+          overflowY: "auto",
         }}
+        popup={true}
+        formats={formats}
+        onView={(newView) => setView(newView)}
         components={{
-          event: Event,
+          event: (eventProps) => (
+            <Event
+              {...eventProps}
+              view={view}
+              onDelete={handleDeleteEvent}
+              onUpdate={handleUpdateEvent}
+            />
+          ),
         }}
-        draggableAccessor={(event) => true}
+        onShowMore={handleShowMore}
       />
     </div>
   );
--- aplicacao-petcare/src/pages/aplicacao-dono-petshop/inicio/Inicio.jsx
+++ aplicacao-petcare/src/pages/aplicacao-dono-petshop/inicio/Inicio.jsx
@@ -2,13 +2,176 @@ import React, { useRef, useState } from "react";
 import UserHeader from "../../../components/aplicacao-dono-petshop/shared/userHeader/UserHeader";
 import styles from "./Inicio.module.css";
 import Calendario from "../../../components/aplicacao-dono-petshop/inicio/calendario/Calendario";
+import TablePagamentos from "../../../components/aplicacao-dono-petshop/inicio/tablePagamentos/TablePagamentos";
+import DropDownFilter from "../../../components/shared/dropDownFilter/DropDownFilter";
+import KpiAgendamentos from "../../../components/aplicacao-dono-petshop/inicio/kpiAgendamentos/KpiAgendamentos";
 
 const Inicio = () => {
+  const dadosPlanos = [
+    {
+      cliente: {
+        nome: "Diogo Souza Moura",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Mensal",
+      },
+      status: "Pendente",
+      PeríodoTitle: "Set/24",
+      Período: "Setembro 2024",
+      valorFaltante: "R$ 100.00",
+    },
+    {
+      cliente: {
+        nome: "Maria Silva",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pendente",
+      PeríodoTitle: "2ªQ Ago/24",
+      Período: "2ª Quinzena Agosto",
+      valorFaltante: "R$ 50.00",
+    },
+    {
+      cliente: {
+        nome: "Julia Cunha",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pago",
+      PeríodoTitle: "1ªQ Set/24",
+      Período: "1ª Quinzena Setembro",
+      valorFaltante: "R$0",
+    },
+    {
+      cliente: {
+        nome: "Julia Cunha",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pago",
+      PeríodoTitle: "1ªQ Set/24",
+      Período: "1ª Quinzena Setembro",
+      valorFaltante: "R$0",
+    },
+    {
+      cliente: {
+        nome: "Julia Cunha",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pago",
+      PeríodoTitle: "1ªQ Set/24",
+      Período: "1ª Quinzena Setembro",
+      valorFaltante: "R$0",
+    },
+    {
+      cliente: {
+        nome: "Julia Cunha",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pago",
+      PeríodoTitle: "1ªQ Set/24",
+      Período: "1ª Quinzena Setembro",
+      valorFaltante: "R$0",
+    },
+    {
+      cliente: {
+        nome: "Maria Silva",
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxw0eitGgbS6Y3kJODK5lGbWxUV8sONkQUZg&s",
+        plano: "Quinzenal",
+      },
+      status: "Pendente",
+      PeríodoTitle: "2ªQ Ago/24",
+      Período: "2ª Quinzena Agosto",
+      valorFaltante: "R$ 50.00",
+    },
+  ];
+
+  const events = [
+    {
+      id: "1",
+      title: "Tosa",
+      paymentStatus: false,
+      paymentMethod: "Dinheiro",
+      start: new Date(2024, 9, 1, 8, 30, 0, 0),
+      end: new Date(2024, 9, 1, 9, 30, 0, 0),
+      status: "Concluído",
+    },
+    {
+      id: "2",
+      title: "Big Meeting",
+      paymentStatus: true,
+      paymentMethod: "Pix",
+      start: new Date(2024, 9, 1, 10, 30, 0, 0),
+      end: new Date(2024, 9, 1, 11, 30, 0, 0),
+      status: "Cancelado",
+    },
+    {
+      id: "3",
+      title: "Tosa",
+      paymentStatus: false,
+      paymentMethod: "Dinheiro",
+      start: new Date(2024, 9, 1, 14, 30, 0, 0),
+      end: new Date(2024, 9, 1, 15, 30, 0, 0),
+      status: "Agendado",
+    },
+    {
+      id: "4",
+      title: "Big Meeting",
+      paymentStatus: true,
+      paymentMethod: "Pix",
+      start: new Date(2024, 9, 1, 17, 30, 0, 0),
+      end: new Date(2024, 9, 1, 18, 0, 0, 0),
+      status: "Concluído",
+    },
+    {
+      id: "5",
+      title: "Vacation",
+      paymentStatus: true,
+      paymentMethod: "Cartão de Crédito",
+      start: new Date(2024, 9, 7, 10, 30, 0, 0),
+      end: new Date(2024, 9, 7, 12, 30, 0, 0),
+      status: "Agendado",
+    },
+    {
+      id: "6",
+      title: "Banho + Tosa",
+      paymentStatus: false,
+      paymentMethod: "Pix",
+      start: new Date(2024, 9, 12, 10, 35, 0, 0),
+      end: new Date(2024, 9, 12, 12, 30, 0, 0),
+      status: "Cancelado",
+    },
+  ];
+
+  const filterOptions = [
+    { label: "Tudo" },
+    { label: "Ontem" },
+    { label: "Hoje" },
+    { label: "Últimos 7 dias" },
+    { label: "Este mês" },
+    { label: "Último mês" },
+  ];
 
   return (
     <div className={styles["container"]}>
       <UserHeader />
-      <Calendario />
+      <div className={styles["main-container"]}>
+        <Calendario
+          dadosAgendamentos={events}
+          className={styles["calendario-container"]}
+        />
+        <div className={styles["kpis-container"]}>
+          <div className={styles["first-container"]}>
+            <DropDownFilter options={filterOptions} />
+            <TablePagamentos dadosPlanos={dadosPlanos} />
+          </div>
+
+          <div className={styles["second-container"]}>
+            <KpiAgendamentos />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
