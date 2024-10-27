d908e58a7a9af9182e8b786a21b0bf3b4ad6d0d6
daniescobarx <daniela.mollo@sptech.school>
2024-10-27 19:07:49 -0300

    style: ajuste barra de progresso de passos do modal

--- aplicacao-petcare/src/components/aplicacao-dono-petshop/cadastroCliente/ModalWrapper.jsx
+++ aplicacao-petcare/src/components/aplicacao-dono-petshop/cadastroCliente/ModalWrapper.jsx
@@ -6,24 +6,49 @@ import styles from './ModalWrapper.module.css';
 
 const ModalWrapper = () => {
   const [currentStep, setCurrentStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true); // Novo estado para controlar a visibilidade do modal
+  const [isOpen, setIsOpen] = useState(true);
 
   const goToNextStep = () => setCurrentStep(currentStep + 1);
   const goToPreviousStep = () => setCurrentStep(currentStep - 1);
   
   const closeModal = () => {
-    setIsOpen(false); // Fecha o modal
-    setCurrentStep(1); // Redefine o passo inicial para 1 quando o modal é fechado
+    setIsOpen(false);
+    setCurrentStep(1);
     window.location.reload();
-
   };
 
-  if (!isOpen) return null; // Não renderiza o modal se ele estiver fechado
+  const renderStepTitles = () => {
+    const steps = [
+      { title: 'Cadastrar Cliente', index: 1 },
+      { title: 'Cadastrar Pet', index: 2 },
+      { title: 'Atribuir Planos', index: 3 },
+    ];
+
+    return (
+      <ul className={styles.stepContainer}>
+        {steps.map((step) => (
+          <li
+            key={step.index}
+            className={`${styles.stepBox} ${step.index === currentStep ? styles.active : ''}`}
+          >
+            {step.index}. {step.title}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const progressWidth = `${(currentStep / 3) * 100}%`; // Calcula a largura da barra de progresso
+
+  if (!isOpen) return null;
 
   return (
     <>
       <div className={styles.modal}>
-        <div className={styles.stepIndicator}>Passo {currentStep}</div>
+        <div className={styles.progressBar}>
+          <div className={styles.progressFill} style={{ width: progressWidth }} />
+        </div>
+        <div className={styles.stepIndicator}>{renderStepTitles()}</div>
         {currentStep === 1 && <Step1Form onNext={goToNextStep} />}
         {currentStep === 2 && <Step2Form onNext={goToNextStep} onBack={goToPreviousStep} />}
         {currentStep === 3 && <Step3Form onBack={goToPreviousStep} />}
