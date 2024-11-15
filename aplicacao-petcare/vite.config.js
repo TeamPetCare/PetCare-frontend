dccd2cd1d7347fb6aa4a8cd824a060a8a1d31920
BeatrizGBrandao <beatriz.brandao@sptech.school>
2024-11-15 12:41:16 -0300

    fix: Ajustando vite.cnfig

--- aplicacao-petcare/vite.config.js
+++ aplicacao-petcare/vite.config.js
@@ -1,9 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
 export default defineConfig({
-    base: '/', // Ajuste conforme o domínio/subdomínio
-    build: {
-        outDir: './dist', // Certifique-se de que está configurado como 'dist'
-    },
-    server: {
-      port: 3000
-    }
+  base: '/', // Ajuste conforme o domínio/subdomínio
+  build: {
+      outDir: './dist', // Certifique-se de que está configurado como 'dist'
+  },
+  server: {
+    port: 3000
+  }
 });
+
