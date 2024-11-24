5ff31b66031587003cbb9aba8efd4134e4a447ad
Daniela Escobar <142854386+daniescobarx@users.noreply.github.com>
2024-11-24 19:46:21 -0300

    style: ajuste layout modal cliente

--- aplicacao-petcare/src/components/aplicacao-dono-petshop/clientesEPets/cadastros/ClientModal.jsx
+++ aplicacao-petcare/src/components/aplicacao-dono-petshop/clientesEPets/cadastros/ClientModal.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import userService from '../../../../services/userService';
-import clientStyles from './ClientModal.module.css'; // Personalizações do ClientModal
-import styles from './ModalWrapper.module.css'; // Estilo genérico do modal
+import clientStyles from './ClientModal.module.css';
+import styles from './ModalWrapper.module.css';
+import InputMask from 'react-input-mask';
 
 const ClientModal = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
@@ -49,103 +50,150 @@ const ClientModal = ({ isOpen, onClose }) => {
 
   return (
     <>
- <div className={`${styles.modal} ${clientStyles.customModal}`}>
-  <h2 className={clientStyles.title}>Cadastrar Cliente</h2>
-  <div className={clientStyles.formGroup}>
-    <div className={clientStyles.row}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Nome Completo"
-        className={clientStyles.fullInput}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div className={clientStyles.row}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="phoneNumber"
-        placeholder="Telefone"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div className={clientStyles.row}>
-      <input
-        type="text"
-        name="cpfClient"
-        placeholder="CPF"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="cep"
-        placeholder="CEP"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div className={clientStyles.row}>
-      <input
-        type="text"
-        name="street"
-        placeholder="Logradouro"
-        className={clientStyles.fullInput}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div className={clientStyles.row}>
-      <input
-        type="text"
-        name="number"
-        placeholder="Número"
-        className={clientStyles.smallInput}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="complement"
-        placeholder="Complemento"
-        className={clientStyles.largeInput}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div className={clientStyles.row}>
-      <input
-        type="text"
-        name="district"
-        placeholder="Bairro"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="city"
-        placeholder="Cidade"
-        className={clientStyles.halfInput}
-        onChange={handleInputChange}
-      />
-    </div>
-  </div>
-  <div className={clientStyles.buttonGroup}>
-    <button className={clientStyles.submitButton} onClick={handleSubmit}>
-      Cadastrar
-    </button>
-    <button className={clientStyles.cancelButton} onClick={onClose}>
-      Cancelar
-    </button>
-  </div>
-</div>
+      <div className={styles.backdrop} onClick={onClose}></div>
+      <div className={`${styles.modal} ${clientStyles.customModal}`}>
+        <h2 className={clientStyles.title}>Cadastrar Cliente</h2>
+        <p className={clientStyles.subtitle}>*Campos obrigatórios</p>
 
+        <div className={clientStyles.formGroup}>
+          {/* Nome e WhatsApp */}
+          <div className={clientStyles.row}>
+            <div className={`${clientStyles.field} ${clientStyles.wideInput}`}>
+              <label className={clientStyles.label}>
+                Nome <span>*</span>
+              </label>
+              <input
+                  placeholder="Nome completo"
+                type="text"
+                name="name"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={`${clientStyles.field} ${clientStyles.narrowInput}`}>
+              <label className={clientStyles.label}>
+                Número de WhatsApp <span>*</span>
+              </label>
+              <InputMask
+                mask="(99) 99999-9999"
+                value={formData.phoneNumber}
+                onChange={(e) => handleInputChange(e)}
+                name="phoneNumber"
+                className={clientStyles.input}
+              />
+            </div>
+          </div>
 
+          {/* Email e CPF */}
+          <div className={clientStyles.row}>
+            <div className={clientStyles.field}>
+              <label className={clientStyles.label}>
+                Email <span>*</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={clientStyles.field}>
+              <label className={clientStyles.label}>
+                CPF <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="cpfClient"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          {/* Endereço */}
+          <h3 className={clientStyles.subtitleAddress}>Endereço</h3>
+          <div className={clientStyles.row}>
+            <div className={`${clientStyles.field} ${clientStyles.narrowInput}`}>
+              <label className={clientStyles.label}>
+                CEP <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="cep"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={`${clientStyles.field} ${clientStyles.wideInput}`}>
+              <label className={clientStyles.label}>
+                Logradouro <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="street"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={`${clientStyles.field} ${clientStyles.narrowInput}`}>
+              <label className={clientStyles.label}>
+                Número <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="number"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          <div className={clientStyles.row}>
+            <div className={`${clientStyles.field} ${clientStyles.narrowInput}`}>
+              <label className={clientStyles.label}>
+                Complemento
+              </label>
+              <input
+                type="text"
+                name="complement"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={`${clientStyles.field} ${clientStyles.wideInput}`}>
+              <label className={clientStyles.label}>
+                Bairro <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="district"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={`${clientStyles.field} ${clientStyles.narrowInput}`}>
+              <label className={clientStyles.label}>
+                Cidade <span>*</span>
+              </label>
+              <input
+                type="text"
+                name="city"
+                className={clientStyles.input}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={clientStyles.buttonGroup}>
+          <button className={clientStyles.cancelButton} onClick={onClose}>
+            Cancelar
+          </button>
+          <button className={clientStyles.submitButton} onClick={handleSubmit}>
+            Cadastrar
+          </button>
+        </div>
+      </div>
     </>
   );
 };
