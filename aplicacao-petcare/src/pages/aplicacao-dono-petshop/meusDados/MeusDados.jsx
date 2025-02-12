fbac91e1ac97a5be6db7951bd99779656d8b295a
leonardoodsilva <leonardo.osilva@sptech.school>
2025-02-12 18:54:27 -0300

    style: frontend base da tela Meus Dados

--- aplicacao-petcare/src/pages/aplicacao-dono-petshop/meusDados/MeusDados.jsx
+++ aplicacao-petcare/src/pages/aplicacao-dono-petshop/meusDados/MeusDados.jsx
@@ -1,39 +1,54 @@
 import Styles from "./MeusDados.module.css";
-import { MdEdit, MdUpload, MdDelete } from "react-icons/md";
+import { MdEdit, MdUpload, MdDelete, MdLock } from "react-icons/md";
 
 const MeusDados = () => {
   return (
     <div className={Styles["container-main"]}>
-      <button className={Styles["custom-btn"]}>
-        <MdEdit /> Editar meus dados
-      </button>
+      <div className={Styles["section01"]}>
+        <button className={Styles["custom-btn"]}>
+          <MdEdit /> Editar meus dados
+        </button>
 
-      {/* Container da foto do usuário e botões */}
-      <div className={Styles["user-photo-container"]}>
-        <div className={Styles["user-photo"]}>
-          <img
-            src="https://blog.casadoprodutor.com.br/wp-content/uploads/2018/04/gatinho.jpg"
-            alt="Foto do Usuário"
-            className={Styles["user-photo-img"]}
-          />
-          <div className={Styles["buttons-container"]}>
-            <button className={Styles["btn-upload"]}>
-              <MdUpload />
-            </button>
-            <button className={Styles["btn-delete"]}>
-              <MdDelete />
-            </button>
+        <div className={Styles["user-photo-container"]}>
+          <div className={Styles["user-photo"]}>
+            <img
+              src="https://blog.casadoprodutor.com.br/wp-content/uploads/2018/04/gatinho.jpg"
+              alt="Foto do Usuário"
+              className={Styles["user-photo-img"]}
+            />
+            <div className={Styles["buttons-container"]}>
+              <button className={Styles["btn-upload"]}>
+                <MdUpload />
+              </button>
+              <button className={Styles["btn-delete"]}>
+                <MdDelete />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className={Styles["container-inputs"]}>
-        <span>Nome*</span>
-        <input type="text" />
-        <span>E-mail</span>
-        <input type="text" />
-        <span>Cargo*</span>
-        <input type="text" />
+
+      <div className={Styles["section02"]}>
+        <div className={Styles["container-inputs"]}>
+          <span>Nome*</span>
+          <input className={Styles["input-nome"]} type="text" />
+          <span>E-mail</span>
+          <input className={Styles["input-e-mail"]} type="text" />
+          <span>Cargo*</span>
+          <select className={Styles["select-cargo"]}>
+            <option value="">Selecione um cargo</option>
+            <option value="gerente">Gerente</option>
+            <option value="funcionario">Funcionário</option>
+          </select>
+        </div>
+
+        <div className={Styles["button-container"]}>
+          <button className={Styles["custom-btn"]}>
+            <MdLock /> Redefinir minha senha
+          </button>
+        </div>
       </div>
+
     </div>
   );
 };
